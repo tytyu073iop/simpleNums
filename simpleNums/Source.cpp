2c9081249aaed284f19363d0fff743c43af165b0
ILYA <ilya.biryuk2005@icloud.com>
2024-02-22 22:12:24 +0300

    Final

--- simpleNums/Source.cpp
+++ simpleNums/Source.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
+#include <vector>
 
 int main() {
 	int x, y;
 	std::cout << "enter number:";
 	std::cin >> x;
+	std::vector<int> a;
 	_asm {
-		mov eax, x // eax = x
-		mov ecx, 2
+		mov eax, x // eax =
+		mov ecx, 1
+		mov edi, eax // buffer
 		cycle1:
-			xor edx, edx
-			mov edi, eax // buffer
+			inc ecx
+			mov eax, edi
+			cdq
 			idiv ecx
-			cmp edx, 0
-			je cycle1
 			cmp eax, 0
-			je exit
+			je exi
+			cmp edx, 0
+			jne cycle1
 			mov y, ecx
+			push eax
+			push ecx
 	}
 	std::cout << y << ", ";
 	_asm {
+		pop ecx
+		pop eax
+		cycle2:
+			mov edi, eax
+			cdq
+			idiv ecx
+			cmp edx, 0
+		je cycle2
 		mov eax, edi
-		inc ecx
 		jmp cycle1
-		exit:
+		exi:
 	}
 }
