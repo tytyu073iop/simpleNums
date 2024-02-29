b6d67a6c8a5447d98747fa3da014780bfbe10d9b
Ilya <ilya.biryuk2005@icloud.com>
2024-02-29 13:41:09 +0300

    Pleasure

--- simpleNums/Biryuk_2.cpp
+++ simpleNums/Biryuk_2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+// find simple
+
 int main() {
 	int x, y;
 	std::cout << "enter number:";
@@ -8,32 +10,32 @@ int main() {
 	std::vector<int> a;
 	_asm {
 		mov eax, x // eax =
-		mov ecx, 1
+		mov ecx, 1 // counter
 		mov edi, eax // buffer
 		cycle1:
 			inc ecx
 			mov eax, edi
 			cdq
 			idiv ecx
-			cmp eax, 0
+			cmp eax, 0 // if end
 			je exi
-			cmp edx, 0
-			jne cycle1
-			mov y, ecx
-			push eax
-			push ecx
+			cmp edx, 0 // if rem zero
+		jne cycle1
+		mov y, ecx
+		push eax // save
+		push ecx
 	}
 	std::cout << y << ", ";
 	_asm {
 		pop ecx
 		pop eax
-		cycle2:
+		cycle2: // clearing
 			mov edi, eax
 			cdq
 			idiv ecx
 			cmp edx, 0
 		je cycle2
-		mov eax, edi
+		mov eax, edi // recover from fail
 		jmp cycle1
 		exi:
 	}
